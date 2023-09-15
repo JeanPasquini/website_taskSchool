3ed9ffa4aa92a2d800cb8ece70be311d67572acf
JeanPasquini <pasquinijean@hotmail.com>
2023-09-15 16:44:37 -0300

    2023-09-15 First commit
    add some files +

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task | Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Task</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="?page=inicio">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="?page=paginaWorkspace">Workspace</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="?page=paginaLogin">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="php/">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+
+
+    <main>
+        <div class="container">
+            <button><a href="?page=paginaLogin">Login</a></button>
+            <button><a href="?page=paginaRegister">Register</a></button>
+        </div>
+    </main>
+
+    <?php
+        //Verifique se a página está definida na URL
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+        } else {
+            $page = 'index'; // Página padrão
+        }
+
+        // Use um switch para determinar qual página incluir com base no valor de $page
+        switch ($page) {
+            case 'paginaLogin':
+                include('php/login.php');
+                break;
+            case 'paginaWorkspace':
+                include('php/workspace.php');
+                break;
+            case 'paginaRegister':
+                include('php/register.php');
+                break;
+            default:
+                include('index.php');
+                break;
+        }
+    ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ php/register.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task | Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style_login/style.css">
+</head>
+<body>
+    <main>
+        <div class="container">
+            <form class="row g-3">
+                <div class="col-md-12">
+                    <label for="inputUsername4" class="form-label">Username</label>
+                    <input type="username" class="form-control" id="inputUsername4">
+                  </div>
+                <div class="col-md-6">
+                  <label for="inputEmail4" class="form-label">Email</label>
+                  <input type="email" class="form-control" id="inputEmail4">
+                </div>
+                <div class="col-md-6">
+                  <label for="inputPassword4" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="inputPassword4">
+                </div>
+                <div class="col-md-6">
+                    <label for="inputEmailOpcional4" class="form-label">Email 2 (Opcional)</label>
+                    <input type="emailOpcional" class="form-control" id="inputEmailOpcional4">
+                  </div>
+                <div class="col-md-4">
+                  <label for="inputRule" class="form-label">Rule</label>
+                  <select id="inputRule" class="form-select">
+                    <option selected>Student</option>
+                    <option>Professor</option>
+                    <option>Teacher</option>
+                  </select>
+                </div>
+                <div class="col-md-2">
+                  <label for="inputZip" class="form-label">Zip</label>
+                  <input type="text" class="form-control" id="inputZip">
+                </div>
+                <div class="col-12">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">
+                      Check me out
+                    </label>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
+              </form>
+        </div>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
